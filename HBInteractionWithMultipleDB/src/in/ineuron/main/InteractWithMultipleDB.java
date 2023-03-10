7c0c00ef4e8efec36d569921897b8b9e3eeaa689
Attraya Das <attrayaghoshdas@gmail.com>
2023-03-10 23:10:13 +0530

    Add HBInteractionWithMultipleDB

---
+++ HBInteractionWithMultipleDB/src/in/ineuron/main/InteractWithMultipleDB.java
@@ -0,0 +1,19 @@
+package in.ineuron.main;
+
+import in.ineuron.dao.ITransferDao;
+import in.ineuron.dao.TransferDaoImpl;
+import in.ineuron.util.MySQLHibernateUtil;
+import in.ineuron.util.OracleHibernateUtil;
+
+public class InteractWithMultipleDB {
+
+	public static void main(String[] args) throws Exception {
+
+		ITransferDao dao = new TransferDaoImpl();
+		System.out.println(dao.transferProductById(1));
+
+		OracleHibernateUtil.closeSessionFactory();
+		MySQLHibernateUtil.closeSessionFactory();
+
+	}
+}
